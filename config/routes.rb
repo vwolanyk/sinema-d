92c5e77ceb46b0164c0ea747c1b6f9a15e7e7946
Vlad Wolanyk <vladwolanyk@gmail.com>
2019-04-27 17:21:15 -0400

    root route, new film form with bootstrap and nested attributes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root "films#index"
   resources :films
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
